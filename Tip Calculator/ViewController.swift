ea6f0888c6a06ebc6933b07658c1e17086fbf0bd
Divyesh Gobiraj <divygobi@gmail.com>
2021-06-19 14:36:24 -0700

    test commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   
+   //testing github
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
